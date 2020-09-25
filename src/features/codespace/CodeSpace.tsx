3a27e9bda4c16c88c70189bb1e14eb413d7fc6a6
deepu105 <d4udts@gmail.com>
2020-09-25 12:51:45 +0200

    show source code in CM

--- src/features/codespace/CodeSpace.tsx
+++ src/features/codespace/CodeSpace.tsx
@@ -1,29 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { UnControlled as CodeMirror } from "react-codemirror2";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Nav from "react-bootstrap/Nav";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/solarized.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/htmlembedded/htmlembedded";
+import "codemirror/mode/handlebars/handlebars";
+import "codemirror/mode/jsx/jsx";
+import "codemirror/mode/vue/vue";
+import "codemirror/mode/php/php";
+import "codemirror/mode/go/go";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/python/python";
+import "codemirror/mode/ruby/ruby";
+import "codemirror/mode/swift/swift";
 
 import "./CodeSpace.scss";
 import { RootState } from "../../app/store";
-import { getOptionsFile, getSelected, SourceFile } from "./codeSpaceSlice";
+import { SourceFile } from "./codeSpaceSlice";
 
 const cmOptions = {
   lineNumbers: true,
-  //   mode: "jdl",
-  //   matchBrackets: true,
-  //   autoCloseBrackets: true,
-  //   keyMap: "sublime",
-  //   extraKeys: {
-  //     "Ctrl-Space": "autocomplete",
-  //   },
+  theme: `solarized dark`,
 };
 
 function CodeSpace() {
   const sourceFiles = useSelector((state: RootState) => state.codeSpace.sourceFiles);
   const [source, setSource] = useState<SourceFile>();
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (sourceFiles && sourceFiles[0]) {
@@ -48,11 +55,10 @@ function CodeSpace() {
         // ref={this.editorRef}
         className="code-mirror-editor"
         value={source?.content}
-        // onBeforeChange={this.editorBeforeChange}
         // onChange={this.editorOnChange}
         options={{
           ...cmOptions,
-          theme: `solarized dark`,
+          mode: findCMMode(source?.name),
         }}
       />
     </div>
@@ -60,3 +66,45 @@ function CodeSpace() {
 }
 
 export default CodeSpace;
+
+function findCMMode(fileName: string | undefined): string {
+  if (!fileName) return "";
+  const parts = fileName.split(".");
+  const ext = parts.length > 0 ? parts[parts.length - 1] : "";
+  switch (ext) {
+    case "html":
+      return "text/html";
+    case "css":
+      return "text/css";
+    case "js":
+      return "text/javascript";
+    case "ts":
+      return "text/typescript";
+    case "jsx":
+      return "text/jsx";
+    case "tsx":
+      return "text/typescript-jsx";
+    case "vue":
+      return "text/x-vue";
+    case "java":
+      return "text/x-java";
+    case "kt":
+      return "text/x-kotlin";
+    case "cs":
+      return "text/x-csharp";
+    case "cshtml":
+      return "text/x-aspx";
+    case "go":
+      return "text/x-go";
+    case "php":
+      return "text/x-php";
+    case "py":
+      return "text/x-python";
+    case "rb":
+      return "text/x-ruby";
+    case "swift":
+      return "text/x-swift";
+    default:
+      return "";
+  }
+}
