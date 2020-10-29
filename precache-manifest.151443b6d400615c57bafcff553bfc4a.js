890c8ffd049cfcac6651e7d684cd1140870a1553
deepu105 <d4udts@gmail.com>
2020-10-29 14:58:43 +0100

    add build

---
+++ precache-manifest.151443b6d400615c57bafcff553bfc4a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42812bc91b830e2f89e81ee91daad47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcc798f2cd63d152736",
+    "url": "/static/css/2.41ea3651.chunk.css"
+  },
+  {
+    "revision": "2d2b54b71771fca8d6f8",
+    "url": "/static/css/main.e58a6b4b.chunk.css"
+  },
+  {
+    "revision": "1fcc798f2cd63d152736",
+    "url": "/static/js/2.347b707b.chunk.js"
+  },
+  {
+    "revision": "b77a62c2f0dce35513c08e3872fecda7",
+    "url": "/static/js/2.347b707b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2b54b71771fca8d6f8",
+    "url": "/static/js/main.b604b7d8.chunk.js"
+  },
+  {
+    "revision": "9334bd66e0491abb088f",
+    "url": "/static/js/runtime-main.6bc068fe.js"
+  },
+  {
+    "revision": "198575332704465d5ffa16912c8ac7d2",
+    "url": "/static/media/adyen-logo.19857533.svg"
+  },
+  {
+    "revision": "f0254ce20e56baacb913846f59b87310",
+    "url": "/static/media/docs-icon.f0254ce2.svg"
+  },
+  {
+    "revision": "c463d965c12a365bcf83b2a8e7563c5d",
+    "url": "/static/media/support-icon.c463d965.svg"
+  }
+]);
--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module "react-split";
-declare module "react-resize-panel";
-declare module "uuid";
-declare module "github-fetch-file";
-declare module "react-syntax-highlighter";
-declare module "react-syntax-highlighter/dist/esm/styles/prism";
